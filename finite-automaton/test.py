cf580d73f1ec34ba241559a1b76372de6ccac77b
nkzlkk <snowcrash@tuta.io>
2020-02-08 19:52:09 +0100

    (#004) Added basic tests for accepting

--- finite-automaton/test.py
+++ finite-automaton/test.py
@@ -1,34 +1,51 @@
 from dfa import *
+import pytest
 
-# VERY UGLY INITIAL
 
-alphabet = {"a", "b", "c"}
-m = DFAutomaton(alphabet)
-qinit = State("q0", False, True)
-q1 = State("q1")
-q2 = State("q2")
-qfinal = State("q3", True)
-qinit.addEdge("a", q1)
-qinit.addEdge("b", qinit)
-q1.addEdge("a", q2)
-q1.addEdge("b", qinit)
-q2.addEdge("a", qfinal)
+ALPHABET = {"a", "b", "c"}
+DFA = DFAutomaton(ALPHABET)
+Q_INIT = State("q0", False, True)
+DFA.addState(Q_INIT)
+Q1 = State("q1")
+Q2 = State("q2")
+Q_FINAL = State("q3", True)
+Q_INIT.addEdge("a", Q1)
+Q_INIT.addEdge("b", Q_INIT)
+Q1.addEdge("a", Q2)
+Q1.addEdge("b", Q_INIT)
+Q2.addEdge("a", Q_FINAL)
+DFA.addState(Q1)
+DFA.addState(Q2)
+# DFA.addState(q3)
+DFA.addState(Q_FINAL)
+# DFA.printAutomaton()
+
+def test_accepting():
+    assert accepts("babaaa", DFA)
+    assert accepts("aaa", DFA)
+    assert accepts("abbbbbbbbabaaa", DFA)
+
+def test_not_accepting():
+    assert not accepts("babaa", DFA)
+    assert not accepts("a", DFA)
+    assert not accepts("aaabaaaa", DFA)
+
+def test_state_not_in_alphabet():
+    m = DFAutomaton(ALPHABET)
+    q = State("q", True, True)
+    s = State("s", True)
+    q.addEdge("g", s)
+    with pytest.raises(exceptions.AlphabetError):
+        m.addState(q)
+        
 
 # node outside alphabet
 # q3 = State()
 # q2.addEdge("d", q3)
 
-m.addNode(qinit)
-m.addNode(q1)
-m.addNode(q2)
-# m.addNode(q3)
-m.addNode(qfinal)
 
-m.printAutomaton()
-
-print("yeet")
-print(accepts("babaaa", m))
-print(accepts("babaab", m))
-print(accepts("a", m))
-print(accepts("aaaaaaaaaaa", m))
-print(accepts("aaa", m))
+# print(accepts("babaaa", DFA))
+# print(accepts("babaab", DFA))
+# print(accepts("a", DFA))
+# print(accepts("aaaaaaaaaaa", DFA))
+# print(accepts("aaa", DFA))
